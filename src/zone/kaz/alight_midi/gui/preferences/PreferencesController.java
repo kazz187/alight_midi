41ec8151c3515374557a1394c5665555ed778962
kazz187 <kazzone87@gmail.com>
2015-09-30 02:38:31 +0900

    Implement selection interfaces for midi device info.

--- src/zone/kaz/alight_midi/gui/preferences/PreferencesController.java
+++ src/zone/kaz/alight_midi/gui/preferences/PreferencesController.java
@@ -1,15 +1,19 @@
 package zone.kaz.alight_midi.gui.preferences;
 
+import com.sun.media.sound.MidiInDeviceProvider;
+import com.sun.media.sound.MidiOutDeviceProvider;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SplitMenuButton;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
 
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiSystem;
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,6 +23,9 @@ import java.util.ResourceBundle;
  */
 public class PreferencesController implements Initializable {
 
+    private static final Class MIDI_IN = MidiInDeviceProvider.class;
+    private static final Class MIDI_OUT = MidiOutDeviceProvider.class;
+
     @FXML
     private Parent root;
     @FXML
@@ -26,24 +33,45 @@ public class PreferencesController implements Initializable {
     @FXML
     private Tab preferencesMidi;
     @FXML
-    private SplitMenuButton preferencesMidiInput;
+    private ComboBox<MidiDevice.Info> preferencesMidiInput;
     @FXML
-    private SplitMenuButton preferencesMidiOutput;
+    private ComboBox<MidiDevice.Info> preferencesMidiOutput;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if (preferences == null) return;
+        initializePreferencesMidi();
         preferences.getSelectionModel().selectedItemProperty().addListener(
                 new ChangeListener<Tab>() {
                     @Override
                     public void changed(ObservableValue<? extends Tab> observable, Tab oldValue, Tab newValue) {
-                        System.out.println(newValue.getId());
-                        if (newValue.getId().equals("preferencesMidi")) {
-                            preferencesMidiInput.getItems().addAll(new MenuItem("hoge"));
+                        if (newValue.getId() != null) {
+                            switch (newValue.getId()) {
+                                case "preferencesMidi":
+                                    prepareDeviceList();
+                                    break;
+                                default:
+                                    break;
+                            }
                         }
                     }
                 }
         );
+    }
+
+    private void initializePreferencesMidi() {
+        StringConverter<MidiDevice.Info> converter = new StringConverter<MidiDevice.Info>() {
+            @Override
+            public String toString(MidiDevice.Info object) {
+                return object.getName() + " - " + object.getDescription();
+            }
+
+            @Override
+            public MidiDevice.Info fromString(String string) {
+                return null;
+            }
+        };
+        preferencesMidiInput.setConverter(converter);
+        preferencesMidiOutput.setConverter(converter);
     }
 
     @FXML
@@ -51,10 +79,27 @@ public class PreferencesController implements Initializable {
         System.out.println("midi selected");
     }
 
-/*    public PreferencesController(Parent root) {
-        this.root = root;
-    }*/
-
-
+    private void prepareDeviceList() {
+        MidiDevice.Info[] devices = MidiSystem.getMidiDeviceInfo();
+        ObservableList<MidiDevice.Info> inputItems = preferencesMidiInput.getItems();
+        ObservableList<MidiDevice.Info> outputItems = preferencesMidiOutput.getItems();
+        for (MidiDevice.Info device : devices) {
+            if (device.getClass().getEnclosingClass().equals(MIDI_IN)) {
+                if (!inputItems.contains(device)) {
+                    inputItems.add(device);
+                }
+            } else if (device.getClass().getEnclosingClass().equals(MIDI_OUT)) {
+                if (!outputItems.contains(device)) {
+                    outputItems.add(device);
+                }
+            }
+        }
+        if (preferencesMidiInput.getValue() == null && inputItems.size() > 0) {
+            preferencesMidiInput.setValue(inputItems.get(0));
+        }
+        if (preferencesMidiOutput.getValue() == null && outputItems.size() > 0) {
+            preferencesMidiOutput.setValue(outputItems.get(0));
+        }
+    }
 
 }
