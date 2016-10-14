e7f6d2f3ce1edfa0f14f785a6d37abb8f216f857
kazz187 <kazzone87@gmail.com>
2016-10-14 18:36:51 +0900

    Support to save and load pad.

--- src/main/java/zone/kaz/alight_midi/gui/sequencer/PadGroup.java
+++ src/main/java/zone/kaz/alight_midi/gui/sequencer/PadGroup.java
@@ -1,11 +1,21 @@
 package zone.kaz.alight_midi.gui.sequencer;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.scene.control.Tab;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import zone.kaz.alight_midi.sequencer.StepSequencerManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PadGroup {
     private final StepSequencerController controller;
@@ -16,13 +26,18 @@ public class PadGroup {
 
     private PadButton[][] padButtons = new PadButton[PAD_NUM_WIDTH][PAD_NUM_HEIGHT];
 
+    private String groupName = "";
+
     public PadGroup(StepSequencerController stepSequencerController, StepSequencerManager stepSequencerManager) {
         this.controller = stepSequencerController;
         this.manager = stepSequencerManager;
     }
 
     void createPadButtons(Tab tab) {
+        groupName = tab.getText();
         AnchorPane pane = (AnchorPane) tab.getContent();
+        String filePath = StepSequencerController.PAD_DIR_PATH + "/" + groupName + ".json";
+        String[][] padInfo = loadPadInfo(filePath);
         pane.getChildren().forEach(child -> {
             GridPane p = (GridPane) child;
             for (int i = 0; i < PAD_NUM_HEIGHT; i++) {
@@ -46,6 +61,7 @@ public class PadGroup {
                                         sequencerInfoData,
                                         controller.getPatternInfo(sequencerInfoData[1])
                                 );
+                                savePad();
                                 success = true;
                             }
                         }
@@ -56,10 +72,59 @@ public class PadGroup {
                     padButton.getLabel().setOnDragDropped(event -> padButton.getShape().fireEvent(event));
                     p.add(padButton.getShape(), j, i);
                     p.add(padButton.getLabel(), j, i);
+                    if (padInfo[j][i] != null) {
+                        PatternInfo patternInfo = controller.getPatternInfo(padInfo[j][i]);
+                        if (patternInfo != null) {
+                            padButton.loadPattern(patternInfo);
+                        }
+                    }
                     padButtons[j][i] = padButton;
                 }
             }
         });
+    }
+
+    private String[][] loadPadInfo(String filePath) {
+        String[][] padInfo = new String[PAD_NUM_WIDTH][PAD_NUM_HEIGHT];
+        try {
+            String json = Files.readAllLines(Paths.get(filePath)).stream().collect(Collectors.joining());
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<List<String>> padInfoTmp = objectMapper.readValue(json, new TypeReference<List<List<String>>>() {});
+            int j = 0;
+            for (List<String> padList : padInfoTmp) {
+                int i = 0;
+                for (String animationName : padList) {
+                    padInfo[j][i] = animationName;
+                    i++;
+                }
+                j++;
+            }
+        } catch (NoSuchFileException e) {
+            // skip
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return padInfo;
+    }
+
+    private void savePad() {
+        String[][] loadedPatternList = createLoadedPatternList();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File(StepSequencerController.PAD_DIR_PATH + "/" + groupName + ".json"), loadedPatternList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String[][] createLoadedPatternList() {
+        String[][] loadedPatternList = new String[PAD_NUM_WIDTH][PAD_NUM_HEIGHT];
+        for (int i = 0; i < PAD_NUM_HEIGHT; i++) {
+            for (int j = 0; j < PAD_NUM_WIDTH; j++) {
+                loadedPatternList[j][i] = padButtons[j][i].getLabel().getText();
+            }
+        }
+        return loadedPatternList;
     }
 
     public PadButton getPadButton(int x, int y) {
